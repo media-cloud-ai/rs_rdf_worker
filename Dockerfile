221d387dccddcab0cad822b5d53bd8bd52a50e9a
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-11-16 12:54:13 +0100

    split and update unit test

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,7 @@ COPY --from=builder /usr/local/cargo/bin/rdf_worker /usr/bin
 
 RUN apt update && apt install -y libssl1.1 ca-certificates
 
+ENV AMQP_QUEUE job_rdf
+ENV AMQP_COMPLETED_QUEUE job_rdf_completed
+ENV AMQP_ERROR_QUEUE job_rdf_error
 CMD rdf_worker
--- src/message.rs
+++ src/message.rs
@@ -30,50 +30,9 @@ pub fn process(message: &str) -> Result<u64, MessageError> {
   match parsed {
     Ok(content) => {
       println!("{:?}", content);
-
       let parameters = content.parameters.clone();
-      let url = "https://gatewayvf.webservices.francetelevisions.fr/v1/videos/".to_owned() + &parameters.reference;
-
-      let client = reqwest::Client::builder()
-        .build()
-        .unwrap();
-
-      let mut response =
-        client
-        .get(url.as_str())
-        .send()
-        .map_err(|e|
-          MessageError::ProcessingError(content.job_id.clone(), e.to_string())
-        )?;
-
-      let status = response.status();
-
-      if !(status == StatusCode::Ok) {
-        println!("ERROR {:?}", response);
-        return Err(MessageError::ProcessingError(content.job_id.clone(), "bad response status".to_string()));
-      }
-
-      let metadatas : Metadata =
-        response.json()
-        .map_err(|e|
-          MessageError::ProcessingError(content.job_id.clone(), e.to_string())
-        )?;
-
-      println!("{:?}", metadatas);
-
-      let mut graph = Graph::new(None);
-      graph.add_namespace(&Namespace::new("rdf".to_string(), Uri::new(RDF_NAMESPACE.to_owned())));
-      graph.add_namespace(&Namespace::new("rdfs".to_string(), Uri::new(RDFS_NAMESPACE.to_owned())));
-      graph.add_namespace(&Namespace::new("owl".to_string(), Uri::new(OWL_NAMESPACE.to_owned())));
-      graph.add_namespace(&Namespace::new("ebucore".to_string(), Uri::new(EBUCORE_NAMESPACE.to_owned())));
-      graph.add_namespace(&Namespace::new("francetv".to_string(), Uri::new(FRANCETV_NAMESPACE.to_owned())));
-      graph.add_namespace(&Namespace::new("xsi".to_string(), Uri::new(XSI_NAMESPACE.to_owned())));
-      graph.add_namespace(&Namespace::new("default".to_string(), Uri::new(DEFAULT_NAMESPACE.to_owned())));
-
-      metadatas.to_rdf(&mut graph);
-      let writer = NTriplesWriter::new();
-      let dumped = writer.write_to_string(&graph).unwrap();
-      println!("{}", dumped);
+      let video_metadata = get_video_metadata(content.job_id.clone(), &parameters.reference)?;
+      let _rdf_triples = convert_into_rdf(content.job_id.clone(), &video_metadata)?;
 
       Ok(content.job_id)
     },
@@ -84,22 +43,52 @@ pub fn process(message: &str) -> Result<u64, MessageError> {
   }
 }
 
+pub fn get_video_metadata(job_id: u64, reference: &str) -> Result<Metadata, MessageError> {
+  let url = "https://gatewayvf.webservices.francetelevisions.fr/v1/videos/".to_owned() + reference;
+
+  let client = reqwest::Client::builder()
+    .build()
+    .unwrap();
+
+  let mut response =
+    client
+    .get(url.as_str())
+    .send()
+    .map_err(|e|
+      MessageError::ProcessingError(job_id, e.to_string())
+    )?;
+
+  let status = response.status();
+
+  if !(status == StatusCode::OK) {
+    println!("ERROR {:?}", response);
+    return Err(MessageError::ProcessingError(job_id, "bad response status".to_string()));
+  }
+
+  response.json()
+  .map_err(|e|
+    MessageError::ProcessingError(job_id, e.to_string())
+  )
+}
+
+pub fn convert_into_rdf(job_id: u64, metadata: &Metadata) -> Result<String, MessageError> {
+  let mut graph = Graph::new(None);
+  graph.add_namespace(&Namespace::new("rdf".to_string(), Uri::new(RDF_NAMESPACE.to_owned())));
+  graph.add_namespace(&Namespace::new("rdfs".to_string(), Uri::new(RDFS_NAMESPACE.to_owned())));
+  graph.add_namespace(&Namespace::new("owl".to_string(), Uri::new(OWL_NAMESPACE.to_owned())));
+  graph.add_namespace(&Namespace::new("ebucore".to_string(), Uri::new(EBUCORE_NAMESPACE.to_owned())));
+  graph.add_namespace(&Namespace::new("francetv".to_string(), Uri::new(FRANCETV_NAMESPACE.to_owned())));
+  graph.add_namespace(&Namespace::new("xsi".to_string(), Uri::new(XSI_NAMESPACE.to_owned())));
+  graph.add_namespace(&Namespace::new("default".to_string(), Uri::new(DEFAULT_NAMESPACE.to_owned())));
+
+  metadata.to_rdf(&mut graph);
+  let writer = NTriplesWriter::new();
+  writer.write_to_string(&graph).map_err(|e| MessageError::ProcessingError(job_id, e.to_string()))
+}
+
+
 #[test]
 fn test_mapping() {
-  use amqp_worker::MessageError::{ProcessingError, RuntimeError};
-
-  let job = "{\"job_id\": 666, \"parameters\": { \"reference\": \"a570fa6f-d2c9-455d-bacf-f21b4957809f\"}}";
-  // let job = "{\"job_id\": 666, \"parameters\": { \"reference\": \"08c8dc14-b831-4bb1-b853-6d883f97c939\"}}";
-  if let Err(error) = process(job) {
-    match error {
-      RuntimeError(msg) => {
-        println!("ERROR {:?}", msg);
-      },
-      ProcessingError(_job_id, msg) => {
-        println!("ERROR {:?}", msg);
-      }
-      _ => {},
-    }
-  }
-  assert!(false);
+  let video_metadata = get_video_metadata(666, "a570fa6f-d2c9-455d-bacf-f21b4957809f").unwrap();
+  let _rdf_triples = convert_into_rdf(666, &video_metadata).unwrap();
 }
