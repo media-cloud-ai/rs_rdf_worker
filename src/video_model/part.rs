0ff76d1f349b8140b27e76a1d101e3e94ee77ab0
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-02-28 19:05:18 +0100

    update part model

--- src/video_model/part.rs
+++ src/video_model/part.rs
@@ -3,7 +3,7 @@ pub struct Part {
     title: Option<String>,
     start: Option<String>,
     duration: Option<String>,
-    index: u32,
+    index: Option<u32>,
     #[serde(default)]
     tags: Vec<String>,
 }
